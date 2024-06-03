330423a3757ef7e8ed266372d9be31ec01c781d8
Engulfedleader2 <engulfedleader@gmail.com>
2024-06-03 10:48:09 -0400

    Initial Commit

---
+++ Midflorida OLB Clone/Midflorida_OLB_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Midflorida_OLB_CloneApp.swift
+//  Midflorida OLB Clone
+//
+//  Created by Israel on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Midflorida_OLB_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
